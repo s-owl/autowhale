f8a13d9dbd5c36aa2f3cfc30b9af29b6599fd8ec
sukso96100 <sukso96100@gmail.com>
2016-10-21 18:52:26 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// Import required modules
+const fs = require('fs');
+var config;
+var Dockerode = require('dockerode');
+var docker = new Dockerode();
+
+while(true){
+  // List Services
+  docker.listServices(function(err, data){
+
+  });
+}
