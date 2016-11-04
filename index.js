69eb82f8fb4614160aa5f94401c955375943062a
sukso96100 <sukso96100@gmail.com>
2016-11-04 17:59:45 +0900

    Added Swarm Service Filtering

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 
 // Import required modules
 const fs = require('fs');
-var config;
+
+// Config file path
+const configPath = 'config.example.json';
+var configs;
 var Dockerode = require('dockerode');
 var docker = new Dockerode();
 
-while(true){
-  // List Services
-  docker.listServices(function(err, data){
 
+scaleServices();
+
+function scaleServices(){
+  loadConfig
+    .then(getFilteredServices)
+    .then()
+
+}
+
+var loadConfig = function(){
+  return new new Promise(function(resolve, reject) {
+    fs.readFile(configPath, (err, data)=>{
+      if(err != undefined){
+        reject(err);
+      }else{
+        resolve(data);
+      }
+    });
   });
+}
+
+var getFilteredServices = function(configs){
+  return new new Promise(function(resolve, reject) {
+    // Get Available Docker Swarm Service List
+    docker.listServices((err, services)=>{
+      if(err != undefined){
+        reject(err);
+      }else{
+        // Filter Services
+        // Leave Only Configuered
+        var configured = services.filter((service)=>{
+          var isConfigured = configs.filter((config)=>{
+            return config.name == service.Spec.Name;
+          });
+          return isConfigured.length == 1;
+        });
+
+        // Leave Only Replicated Services
+        var replicated = configured.filter((service)=>{
+          return service.Spec.Mode.Replicated != undefined;
+        });
+
+        resolve([replicated, configs]);
+      }
+    });
+  });
+}
+
+function getByName() {
+
 }
